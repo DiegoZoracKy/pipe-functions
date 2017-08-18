33531f027a2dcea6355d3a279b58e1570e0eb371
Diego Oliveira <diego.oliveira@elecnor-celeo.local>
2017-08-18 15:18:14 -0300

    Improved to handle one single input value or function

--- tests/main.test.js
+++ tests/main.test.js
@@ -17,9 +17,16 @@ describe('Pipe', function() {
 			assert.equal(result, `"Time"`);
 		});
 
-		it(`should pass forward 'undefined' values`, function() {
+		it(`should work with even with only one value being input`, function() {
+			assert.equal(pipe('Z'), 'Z');
+		});
 
-			const a = () => new Promise((resolve, reject) => setTimeout(() => resolve(undefined), 1000));;
+		it(`should work with even with only one function being input`, function() {
+			assert.equal(pipe(x => 'K'), 'K');
+		});
+
+		it(`should pass forward 'undefined' values`, function() {
+			const a = () => undefined;
 			const b = (x) => x + undefined;
 			const c = (x) => x + 'C';
 
@@ -31,19 +38,24 @@ describe('Pipe', function() {
 	describe('Functions && Promises (sync && async)', function() {
 
 		it(`'time' should be turned into 'Pink Floyd - Time'`, function(done) {
-
 			pipe('time', capitalize, fetchAndSetBandName)
 				.then(result => assert.equal(`Pink Floyd - Time`, result))
 				.then(() => done());
 		});
 
 		it(`should pass forward 'undefined' values`, function() {
-
-			const a = () => undefined;
+			const a = () => new Promise((resolve, reject) => setTimeout(() => resolve(undefined), 1000));
 			const b = (x) => x + undefined;
 			const c = (x) => x + 'C';
 
 			assert(pipe(a, b, c));
+		});
+
+		it(`should work with even with only one function being input`, function(done) {
+			const promise = () => new Promise((resolve, reject) => setTimeout(() => resolve('Z'), 1000));
+			pipe(promise)
+				.then(result => assert.equal(`Z`, result))
+				.then(() => done());
 		});
 
 	});
